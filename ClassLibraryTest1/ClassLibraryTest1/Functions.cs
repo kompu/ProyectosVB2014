293f1063d091844f59e770091e7d9d09db6a5e2e
Felipe V <Felipevillarus@live.com>
2014-02-12 00:32:09 +0400

    12/02/2014
    Creacion del repositorio, y hoy, creacion de el proyecto WindowsForms
    funcional solo las dos primeras funciones.

---
+++ ClassLibraryTest1/ClassLibraryTest1/Functions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryTest1
+{
+    public class Functions
+    {
+        /// <summary>
+        /// формула y = aX2 + bX + c
+        /// </summary>
+        /// <param name="a">параметр A</param>
+        /// <param name="b">параметр B</param>
+        /// <param name="c">параметр C</param>
+        /// <param name="x">параметр X</param>
+        /// <returns>возвращает double формула y = aX2 + bX + c</returns>
+        public double Polinom(double a, double b, double c, double x)
+        {
+            double y = (a * a * x) + (b * x) + c;
+            return y;
+        }
+
+        /// <summary>
+        /// формула y = aX2 + bX + c + d
+        /// </summary>
+        /// <param name="a">параметр A</param>
+        /// <param name="b">параметр B</param>
+        /// <param name="c">параметр C</param>
+        /// <param name="x">параметр X</param>
+        /// <param name="d">параметр D</param>
+        /// <returns>возвращает double формула y = aX2 + bX + c + d</returns>
+        public double Polinom(double a, double b, double c, double x, double d)
+        {
+            double y = (a * a * x) + (b * x) + c + d;
+            return y;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibraryTest1;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("привет всем");
+            double a, b, c, x;
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            x = double.Parse(Console.ReadLine());
+            Functions obje = new Functions();
+            double y = obje.Polinom(a, b, c, x);
+            Console.WriteLine("y = aX^2 + bX + C " + "y = " + y);
+            Console.ReadLine();
+        }
+    }
+}
